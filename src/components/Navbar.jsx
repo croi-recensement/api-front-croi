54a749935bf316ae574230f7f19b98424efa12cd
croi-recensement <croi-recensement@gmail.com>
2021-04-08 14:48:49 +0200

    route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,9 +11,12 @@ const Navbar = () =>{
             <div className="collapse navbar-collapse" id="navbarColor01">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home
+                        <a className="nav-link" href="/">Accueil
                         <span className="sr-only">(current)</span>
                         </a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/inscription">Inscription</a>
                     </li>
                 </ul>
             </div>
